490c9cf00419b6b86be2d95123e76f4218ec166b
JIN-YEONG <53157312+JIN-YEONG@users.noreply.github.com>
2019-08-01 15:40:04 +0900

    Update keras.Test01.py

--- kerastest/keras.Test01.py
+++ kerastest/keras.Test01.py
@@ -23,7 +23,9 @@ file_data.drop('Unnamed: 7' ,axis=1, inplace=True)
 
 file_data=file_data.sort_values(by=['일자']) # 시간순으로 정렬
 file_data.drop('일자', axis=1, inplace=True)
-
+# 추가사항
+# file_data.drop('거래량', axis=1, inplace = True)
+# file_data.drop('환율(원/달러)', axis=1, inplace = True)
 
 price = file_data.values[:,:-1]
 scaler = MinMaxScaler()
@@ -36,7 +38,7 @@ mm_volume = scaler.transform(volume)
 
 
 x = np.concatenate((mm_price, mm_volume), axis=1)
-y = file_data['종가']
+y = file_data['종가']   # y = x[:,3]   # 수정사항
 
 
 data_x = []
@@ -72,7 +74,7 @@ val_x= val_x.reshape(119,5*6)
 # print(val_y.shape)
 
 model = Sequential()
-# model.add(LSTM(119, batch_input_shape=(batchSize,5,6), stateful=True))
+# model.add(LSTM(119, batch_input_shape=(batchSize,5,4), stateful=True))   # 수정사항 , batch_input_shape 값 변경
 
 model.add(Dense(128, input_shape=(5*6, ),activation='relu'))
 model.add(BatchNormalization())
@@ -104,10 +106,47 @@ early_stopping = EarlyStopping(monitor='vol_mse', patience=20, mode='auto')
 
 model.fit(train_x, train_y, epochs=repeat_num, batch_size=batchSize, verbose=2, validation_data=(val_x, val_y), callbacks=[early_stopping])
 
+# 추가 사항 
+# # 상태유지 LSTM용
+# for rp_id in range(repeat_num):
+#     print('num:' + str(rp_id))
+#     model.fit(train_x, train_y, 
+#         epochs=1, batch_size=batchSize, verbose=2, 
+#         shuffle=False, validation_data=(val_x, val_y), callbacks=[early_stopping]
+#     )
+#     model.reset_states()
+
+
+
 
 loss, mse = model.evaluate(test_x, test_y, batch_size=batchSize)
 print('mse:', mse)
 model.reset_states()
 
 y_predict = model.predict(test_x[-1], batch_size=batchSize)
+# 추가 사항
+# y_predict = scaler.inverse_transform(y_predict)
+# y_predict = np.mean(y_predict)
 print(y_predict)
+
+'''
+1. 문제점 파악
+   - 모델의 최적화 부족
+   - 많은 데이터
+   - 불필요한 데이터 포함
+   - 여러개의 데이터 출력
+2. 개선책
+   - 모델의 최적화 
+   - 3달 정도의 데이터 사용
+   - 불필요한 데이터 제거(일자, 거래량, 환율)
+   - 여러 데이터들의 평균 값을 최종값으로 사용
+3. 판단 지표
+   - mse를 이용한 정확도 판단
+4. 소스 수정
+   - line 26
+   - line 41
+   - line 77
+   - line 80~96 주석 처리
+   - line 109
+   - line 127
+'''
